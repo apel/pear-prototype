db284e84a84e739a5a876329814ba997a1bb8798
Nikolaos Kostas <nikolaos.kostas@cern.ch>
2026-05-20 15:29:11 +0200

    refactor(apel_parser): define accounting data schema constants

--- apel_parser/constants.py
+++ apel_parser/constants.py
@@ -2,6 +2,10 @@ import os
 from typing import Final
 
 
+# ==============================================================================
+# CONSUMED MESSAGES - APEL
+# ==============================================================================
+
 DEFAULT_MESSAGES_DIR: Final[str] = "/var/spool/apel/grid/incoming"
 
 APEL_DIRQ_SCHEMA: Final[dict[str, str]] = {
@@ -10,13 +14,15 @@ APEL_DIRQ_SCHEMA: Final[dict[str, str]] = {
     "empaid": "string?",
 }
 
+# ==============================================================================
+# TOPOLOGY DATA
+# ==============================================================================
+
 CRIC_RCSITE_API: Final[str] = "https://wlcg-cric.cern.ch/api/core/rcsite/query/?json&state=ANY"
 CRIC_REQUEST_TIMEOUT_SECONDS: Final[int] = 30
 IGTF_TRUST_BUNDLE_PATH: Final[str] = "/cvmfs/grid.cern.ch/etc/grid-security/certificates"
 
 UNKNOWN: Final[str] = "UNKNOWN"
-
-GRID_INFRASTRUCTURE: Final[str] = "Grid"
 
 LHC_VOS: Final[dict[str, str]] = {
     "atlas": "ATLAS",
@@ -44,6 +50,42 @@ DESY_FEDERATIONS: Final[dict[str, dict[str, object]]] = {
     },
 }
 NON_WLCG_FEDERATION: Final[str] = "NON-WLCG-Federation"
+
+# ==============================================================================
+# PRODUCED ACCOUNTING DATA
+# ==============================================================================
+
+GRID_INFRASTRUCTURE: Final[str] = "Grid"
+CLOUD_INFRASTRUCTURE: Final[str] = "Cloud"
+
+COMMON_ACCOUNTING_FIELDS: Final[list[str]] = [
+    "raw_wc_time",
+    "raw_wc_work",
+    "raw_cpu_time",
+    "raw_cpu_work",
+    "raw_cpu_eff",
+]
+GRID_ONLY_ACCOUNTING_FIELDS: Final[list[str]] = [
+    "number_of_jobs",
+]
+CLOUD_ONLY_ACCOUNTING_FIELDS: Final[list[str]] = [
+]
+ALL_ACCOUNTING_FIELDS: Final[list[str]] = COMMON_ACCOUNTING_FIELDS + GRID_ONLY_ACCOUNTING_FIELDS + CLOUD_ONLY_ACCOUNTING_FIELDS
+
+INFLUXDB_TAGS: Final[list[str]] = [
+    "vo", "tier", "country", "federation", "site", "infrastructure", "benchmark"
+]
+
+PRODUCED_DOC_FIELDS: Final[list[str]] = (
+    INFLUXDB_TAGS
+    + ["roc"]
+    + ALL_ACCOUNTING_FIELDS
+    + ["idb_tags", "producer", "type", "timestamp"]
+)
+
+# ==============================================================================
+# PUBLISHED MESSAGES - MONIT
+# ==============================================================================
 
 MQ_CONFIG: Final[dict[str, str | None]] = {
     "host": os.getenv("MQ_HOST"),
